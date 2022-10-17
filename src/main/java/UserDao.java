5e2add47aa27677f89523eed4488fe4c62171662
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-17 13:41:48 +0900

    [update] Select 쿼리를 실행하는 get 메소드 추가 및 User DO, VO 생성

--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -1,3 +1,5 @@
+import domain.User;
+
 import java.sql.*;
 import java.util.Map;
 
@@ -13,20 +15,48 @@ public class UserDao {
         Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
 
         /*DB에 쿼리 입력 후 바인딩*/
-        PreparedStatement ps = conn.prepareStatement("SELECT password FROM users WHERE id = ?");
-        ps.setString(1, "2");
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO users VALUES(?, ?, ?)");
+        ps.setString(1, "1");
+        ps.setString(2, "Seunghwan");
+        ps.setString(3, "1234");
 
         /*쿼리 실행*/
         ResultSet resultSet = ps.executeQuery(); // ctrl + enter
         resultSet.next();
-        System.out.println(resultSet.getString(1));
 
         ps.close(); // 서버 어플리케이션인 경우에는 반드시 작성 필요
         conn.close();
     }
 
+    public User get(String id) throws SQLException, ClassNotFoundException {
+        Map<String, String> env = System.getenv(); // 환경변수를 사용하여
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword = env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+
+        /*DB에 쿼리 입력 후 바인딩*/
+        PreparedStatement ps = conn.prepareStatement("SELECT id, name, password FROM users WHERE id = ?");
+        ps.setString(1, id);
+
+        /*쿼리 실행*/
+        ResultSet resultSet = ps.executeQuery(); // ctrl + enter
+        resultSet.next();
+
+        User user = new User(resultSet.getString("id"), resultSet.getString("name"), resultSet.getString("password"));
+
+        ps.close(); // 서버 어플리케이션인 경우에는 반드시 작성 필요
+        conn.close();
+
+        return user;
+    }
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
-        userDao.add();
+//        userDao.add();
+        User testUser = userDao.get("2");
+        System.out.println(testUser.getName());
     }
 }
